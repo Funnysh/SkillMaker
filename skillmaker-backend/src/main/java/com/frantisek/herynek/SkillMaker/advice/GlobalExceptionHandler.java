40e429d9c020ad4c46a306c45e6a95517966e3d6
František <[herynek.frantisek@gmail.com]>
2025-09-17 12:04:14 +0200

    Přidání komentáře

--- skillmaker-backend/src/main/java/com/frantisek/herynek/SkillMaker/advice/GlobalExceptionHandler.java
+++ skillmaker-backend/src/main/java/com/frantisek/herynek/SkillMaker/advice/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-//Globální zpracovíné výjimek
+//Globální zpracování výjimek
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
